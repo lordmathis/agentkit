0a406643a1f4046716657b20f5c81e36080c2c2d
Matúš Námešný <matus@namesny.com>
2025-12-28 16:31:48 +0100

    Basic web search agent

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
-def main():
-    print("Hello from agents!")
+from smolagents import CodeAgent, DuckDuckGoSearchTool, OpenAIModel
+import os
+from dotenv import load_dotenv
+
+load_dotenv(override=True)
+
+model = OpenAIModel(
+    model_id="gpt-oss-20b",
+    api_base="https://llamactl.nmsny.dev/v1",
+    api_key=os.environ["LLAMACTL_API_KEY"],
+)
+
+agent = CodeAgent(
+    tools=[DuckDuckGoSearchTool()],
+    model=model,
+)
 
 
-if __name__ == "__main__":
-    main()
+# Run the agent with a task
+result = agent.run("What is the current weather in Paris?")
+print(result)
